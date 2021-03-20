1009c5aa7d2a38d83bb1b2900bdbcecc96d914fe
Mark Sto <mstoyukhin@gmail.com>
2021-03-21 01:08:05 +0300

    Fix bot URL detection (for webhook)

--- src/general_expenses_accountant/config.clj
+++ src/general_expenses_accountant/config.clj
@@ -6,10 +6,16 @@
 (cfg/define {:bot-api-token {:description "Telegram Bot API token"
                              :verifier #(= (count %2) 46)
                              :type :string}
+             :bot-url {:description "The bot URL (for a webhook)"
+                       :required false
+                       :type :string}
              :environment {:description "Either 'DEV' or 'PROD'"
                            :one-of #{dev-env "PROD"}
                            :required false
                            :type :string}
+             :heroku-app-name {:description "Heroku app name"
+                               :required false
+                               :type :string}
              :port {:type :number
                     :default 8080}})
 
