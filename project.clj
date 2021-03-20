4864647ba8ac37074de5cd9ba913fd8b11e47fce
Mark Sto <mstoyukhin@gmail.com>
2021-03-20 16:45:58 +0300

    Clean up 'project.clj'

--- project.clj
+++ project.clj
@@ -8,6 +8,9 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "1.3.610"]
 
+                 [com.grammarly/omniconf "0.2.2"]
+                 [com.taoensso/timbre "5.1.2"]
+
                  [fipp "0.6.23"]
 
                  [compojure "1.6.2"]
@@ -22,23 +25,7 @@
 
                  [morse "0.4.3"]
 
-                 [tongue "0.2.10"]
-
-                 ;; TODO: Use these libs as well.
-                 ;; - enable some more ring middleware
-                 ;[metosin/compojure-api "1.1.9"]
-                 ;[metosin/ring-http-response "0.8.0"]
-                 ;[ring.middleware.logger "0.5.0"]
-                 ;[bk/ring-gzip "0.1.1"]
-                 ;; - pick one of these
-                 [com.grammarly/omniconf "0.2.2"]
-                 ;[coldnew/config "1.2.0"]
-                 ;[wrench "0.3.3"]
-                 ;; - set up logging properly
-                 [com.taoensso/timbre "5.1.2"]
-                 ;[org.clojure/tools.logging "0.3.1"]
-                 ;[spootnik/unilog "0.7.27"]
-                 ]
+                 [tongue "0.2.10"]]
 
   :plugins [[lein-ring "0.12.5"]] ;; to run an app locally
   :ring {:handler general-expenses-accountant.handler/app}
