a4e2cb8327578edf89cc05f613a2e14ca63a04c6
Mark Sto <mstoyukhin@gmail.com>
2021-05-15 14:32:19 +0300

    Add a ToDo item for test coverage

--- test/general_expenses_accountant/core_test.clj
+++ test/general_expenses_accountant/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [general-expenses-accountant.core :refer :all]))
 
+;; TODO: Implement the test coverage in a form of externally stored (in files) web requests series (run by scripts).
+
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
